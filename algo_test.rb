a015e493a34c2f388c5444d9f4647fba62478591
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2016-07-02 23:37:45 +0100

    checkpoint

--- algo_test.rb
+++ algo_test.rb
@@ -1,8 +1,9 @@
-require './algo'
-require 'rspec'
-require './spec/spec_helper.rb'
-require 'pry'
-require 'json'
+require "./algo"
+require 'rspec/autorun'
+require "rspec"
+require "./spec/spec_helper.rb"
+require "pry"
+require "json"
 
 def run_functions(solution,function_array)
   function_array.each do |fn_obj|
@@ -10,40 +11,45 @@ def run_functions(solution,function_array)
     y = fn_obj["params"]
     solution = solution.send(x, *y)
   end
+  solution
 end
 
-f = open('challenge-2.json').read
+f = open("challenge-2.json").read
 json = JSON.parse(f)
 problems = json["challenges"]
 
 outputs = []
 
-problems.each do |problem|
-  initialize_params = problem["class_initialize_params"]
-  solution = Pathfinder.new(*initialize_params)
-  run_functions(solution,problem["functions_to_call_on_instance"])
+describe "Pathfinder" do
+  after(:all) do
+    puts outputs.to_json
+  end
+  problems.each do |problem|
+    initialize_params = problem["class_initialize_params"]
+    solution = Pathfinder.new(*initialize_params)
 
-  RSpec.describe "test #{index}" do
-  problem["tests"].each_with_index do |test_obj, index|
-    x = test_obj["check_name"]
-    y = test_obj["value"]
+    problem["tests"].each_with_index do |test_obj, index|
+      context "test #{index}" do
+        x = test_obj["check_name"]
+        y = test_obj["value"]
 
 
-      if x == "equality"
-        it "equality" do
-          result = expect(solution).to(eq(y))
-          outputs << { test_id: index, result: result }
-        end
-      elsif x == "functionCalled"
-        it "functionCalled" do
-          allow(solution).to receive(y.to_sym)
-          run_functions
-          result = expect(solution).to have_received(y.to_sym)
-          outputs << { test_id: index, result: result }
+        if x == "equality"
+          it "equality" do
+            res = run_functions(solution,problem["functions_to_call_on_instance"])
+
+            result = expect(res).to(eq(y))
+            outputs << { test_id: index, result: result }
+          end
+        elsif x == "functionCalled"
+          it "functionCalled" do
+            allow(solution).to receive(y.to_sym)
+            run_functions(solution,problem["functions_to_call_on_instance"])
+            result = expect(solution).to have_received(y.to_sym)
+            outputs << { test_id: index, result: result }
+          end
         end
       end
     end
   end
 end
-
-puts outputs.to_json
